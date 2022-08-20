a1719f2e46fa44e6819d1a53d4d55d22056e34b9
Anders Kaplan <anders.kaplan@magasin1.se>
2022-08-20 23:26:36 +0200

    Markdown renderer: major refactoring

--- contrib/markdown_renderer.py
+++ contrib/markdown_renderer.py
@@ -7,8 +7,10 @@ except for nonessential whitespace.
 
 import os
 import re
-from itertools import chain
+from itertools import chain, repeat
+from string import whitespace
 import sys
+from typing import Iterable, Sequence, Tuple
 
 from mistletoe import block_token, span_token
 from mistletoe.base_renderer import BaseRenderer
@@ -73,147 +75,149 @@ class MarkdownRenderer(BaseRenderer):
         self.render_map["SetextHeading"] = self.render_setext_heading
         self.render_map["CodeFence"] = self.render_fenced_code_block
         self.render_map["LinkReferenceDefinition"] = self.render_link_reference_definition
-        self.indentation = ""
-        self.is_at_beginning_of_line = True
 
-    def indent(self):
-        if self.is_at_beginning_of_line:
-            self.is_at_beginning_of_line = False
-            return self.indentation
-        else:
-            return ""
+    def span_to_lines(self, children: Iterable[span_token.SpanToken]) -> Iterable[str]:
+        # render a sequence of span (inline) tokens to a sequence of lines
+        def walk_tuple_tree(t):
+            if not isinstance(t, tuple):
+                yield t
+            else:
+                for item in t:
+                    for inner in walk_tuple_tree(item):
+                        yield inner
 
-    def indent_and_feed_line(self, line):
-        content = "".join((self.indentation if self.is_at_beginning_of_line else "", line, "\n"))
-        self.is_at_beginning_of_line = True
-        return content
+        current_line = []
+        tree = tuple(map(self.render, children))
+        for w in walk_tuple_tree(tree):
+            if isinstance(w, span_token.LineBreak):
+                current_line.append(getattr(w, "tag", ""))
+                yield "".join(current_line)
+                current_line = []
+            else:
+                current_line.append(w)
+        if len(current_line) > 0:
+            yield "".join(current_line)
 
-    def indent_lines(self, lines):
-        return "".join(map(self.indent_and_feed_line, lines))
+    def block_to_lines(self, children: Iterable[block_token.BlockToken]) -> Iterable[str]:
+        # render a sequence of block tokens to a sequence of lines
+        return chain.from_iterable(map(self.render, children))
+
+    def prefix_lines(self, lines, first_line_prefix, following_line_prefix = None):
+        following_line_prefix = following_line_prefix or first_line_prefix
+        isFirstLine = True
+        for line in lines:
+            if isFirstLine:
+                l = first_line_prefix + line
+                isFirstLine = False
+            else:
+                l = following_line_prefix + line
+            yield l if not l.isspace() else ""
 
     # span/inline tokens
+    # rendered into tuple trees where the leaf nodes are strings or LineBreak tokens.
 
-    def render_raw_text(self, token) -> str:
-        return "".join((self.indent(), token.content))
+    def render_raw_text(self, token: span_token.RawText) -> str:
+        return token.content
 
-    def render_strong(self, token: span_token.Strong) -> str:
-        return "".join((self.indent(), token.tag * 2, self.render_inner(token), token.tag * 2))
+    def render_strong(self, token: span_token.Strong) -> Tuple:
+        return (token.tag * 2, *map(self.render, token.children), token.tag * 2)
 
-    def render_emphasis(self, token: span_token.Emphasis) -> str:
-        return "".join((self.indent(), token.tag, self.render_inner(token), token.tag))
+    def render_emphasis(self, token: span_token.Emphasis) -> Tuple:
+        return (token.tag, *map(self.render, token.children), token.tag)
 
-    def render_inline_code(self, token: span_token.InlineCode) -> str:
-        return "".join((self.indent(), '`', self.render_inner(token), '`'))
+    def render_inline_code(self, token: span_token.InlineCode) -> Tuple:
+        return ('`', *map(self.render, token.children), '`')
 
-    def render_strikethrough(self, token: span_token.Strikethrough) -> str:
-        return "".join((self.indent(), '~~', self.render_inner(token), '~~'))
+    def render_strikethrough(self, token: span_token.Strikethrough) -> Tuple:
+        return ('~~', *map(self.render, token.children), '~~')
 
-    def render_image(self, token: span_token.Image) -> str:
-        return self.render_image_or_link(token, '!', token.src)
+    def render_image(self, token: span_token.Image) -> Tuple:
+        return self.render_image_or_link(token, True, token.src)
 
-    def render_link(self, token: span_token.Link) -> str:
-        return self.render_image_or_link(token, '', token.target)
+    def render_link(self, token: span_token.Link) -> Tuple:
+        return self.render_image_or_link(token, False, token.target)
 
-    def render_image_or_link(self, token, prefix, target):
-        indent = self.indent() # note: must call self.indent() before self.render_inner(token)
+    def render_image_or_link(self, token, isImage, target) -> Tuple:
+        prefix = "![" if isImage else "["
         if token.tag_dest_type == "uri" or token.tag_dest_type == "angle_uri":
-            if token.tag_dest_type == "angle_uri":
-                dest_part = "".join(("<", target, ">"))
-            else:
-                dest_part = target
+            dest_part = "".join(("<", target, ">")) if token.tag_dest_type == "angle_uri" else target
             if len(token.title) > 0:
+                # "![" description "](" dest_part " " title ")"
                 closer = ')' if token.tag_title == '(' else token.tag_title
-                title_part = " {}{}{}".format(token.tag_title, token.title, closer)
+                return (prefix,
+                        *map(self.render, token.children),
+                        "]({} {}{}{})".format(dest_part, token.tag_title, token.title, closer))
             else:
-                title_part = ""
-            return "{}{}[{}]({}{})".format(indent, prefix, self.render_inner(token), dest_part, title_part)
+                # "![" description "](" dest_part ")"
+                return (prefix, *map(self.render, token.children), "](" + dest_part + ")")
+        elif token.tag_dest_type == "full":
+            # "![" description "][" tag_dest "]"
+            return (prefix, *map(self.render, token.children), "][" + token.tag_dest + "]")
+        elif token.tag_dest_type == "collapsed":
+            # "![" description "][]"
+            return (prefix, *map(self.render, token.children), "][]")
         else:
-            if token.tag_dest_type == "full":
-                text_part = "[{}]".format(self.render_inner(token))
-                dest_part = token.tag_dest
-            elif token.tag_dest_type == "collapsed":
-                text_part = "[{}]".format(self.render_inner(token))
-                dest_part = ""
-            else:
-                text_part = ""
-                dest_part = self.render_inner(token)
-            return "{}{}{}[{}]".format(indent, prefix, text_part, dest_part)
+            # "![" description "]"
+            return (prefix, *map(self.render, token.children), "]")
 
-    def render_auto_link(self, token: span_token.AutoLink) -> str:
-        return "".join((self.indent(), "<", self.render_inner(token), ">"))
+    def render_auto_link(self, token: span_token.AutoLink) -> Tuple:
+        return ('<', *map(self.render, token.children), '>')
 
     def render_escape_sequence(self, token: span_token.EscapeSequence) -> str:
-        return "".join((self.indent(), "\\", self.render_inner(token)))
+        return "\\" + token.children[0].content
 
-    def render_line_break(self, token: span_token.LineBreak) -> str:
-        content = "".join((self.indent(), getattr(token, "tag", ""), "\n"))
-        self.is_at_beginning_of_line = True
-        return content
+    def render_line_break(self, token: span_token.LineBreak) -> span_token.LineBreak:
+        return token
 
     def render_html_span(self, token: span_token.HTMLSpan) -> str:
-        return "".join((self.indent(), token.content))
+        return token.content
 
     # block tokens
+    # rendered into sequences of lines (strings), to be joined by newlines.
+    # except render_document, which returns a single string containing the fully rendered document.
 
-    def render_heading(self, token: block_token.Heading) -> str:
+    def render_document(self, token: block_token.Document) -> str:
+        lines = self.block_to_lines(token.children)
+        return "".join(chain.from_iterable(zip(lines, repeat("\n"))))
+
+    def render_heading(self, token: block_token.Heading) -> Iterable[str]:
+        content = next(self.span_to_lines(token.children))
         trailer_part = "".join((" ", token.tag_trailer)) if len(token.tag_trailer) > 0 else ""
-        content = "".join((self.indent(), "#" * token.level, " ", self.render_inner(token), trailer_part, "\n"))
-        self.is_at_beginning_of_line = True
-        return content
+        return ["".join(("#" * token.level, " ", content, trailer_part))]
 
-    def render_setext_heading(self, token: block_token.SetextHeading) -> str:
+    def render_setext_heading(self, token: block_token.SetextHeading) -> Iterable[str]:
         char = "=" if token.level == 1 else "-"
-        content = "".join((self.indent(), self.render_inner(token), "\n", char * token.tag_length, "\n"))
-        self.is_at_beginning_of_line = True
+        content = list(self.span_to_lines(token.children))
+        content.append(char * token.tag_length)
         return content
 
-    def render_quote(self, token: block_token.Quote) -> str:
-        prefix = "".join((self.indent(), "> "))
-        prev_indentation = self.indentation
-        self.indentation += "> "
-        self.is_at_beginning_of_line = False
-        content = "".join((prefix, self.render_inner(token), "\n" if not self.is_at_beginning_of_line else ""))
-        self.is_at_beginning_of_line = True
-        self.indentation = prev_indentation
-        return content
+    def render_quote(self, token: block_token.Quote) -> Iterable[str]:
+        lines = list(self.block_to_lines(token.children))
+        if len(lines) == 0:
+            lines = [""]
+        return self.prefix_lines(lines, "> ")
 
-    def render_paragraph(self, token: block_token.Paragraph) -> str:
-        content = "".join((self.indent(), self.render_inner(token), "\n"))
-        self.is_at_beginning_of_line = True
-        return content
+    def render_paragraph(self, token: block_token.Paragraph) -> Iterable[str]:
+        return self.span_to_lines(token.children)
 
-    def render_block_code(self, token: block_token.BlockCode) -> str:
-        prev_indentation = self.indentation
-        self.indentation += "    "
+    def render_block_code(self, token: block_token.BlockCode) -> Iterable[str]:
         lines = token.children[0].content[:-1].split("\n")
-        content = self.indent_lines(lines)
-        self.indentation = prev_indentation
-        return content
+        return self.prefix_lines(lines, "    ")
 
-    def render_fenced_code_block(self, token: block_token.BlockCode) -> str:
-        prev_indentation = self.indentation
-        self.indentation += " " * token.indentation
-        def make_lines():
-            yield "".join((token.tag, token.info_string))
-            for line in token.children[0].content[:-1].split("\n"):
-                yield line
-            yield token.tag
-        content = self.indent_lines(make_lines())
-        self.indentation = prev_indentation
-        return content
+    def render_fenced_code_block(self, token: block_token.BlockCode) -> Iterable[str]:
+        lines = ["".join((token.tag, token.info_string))]
+        lines.extend(token.children[0].content[:-1].split("\n"))
+        lines.append(token.tag)
+        return self.prefix_lines(lines, " " * token.indentation)
 
-    def render_list(self, token: block_token.List) -> str:
-        return self.render_inner(token)
+    def render_list(self, token: block_token.List) -> Iterable[str]:
+        return self.block_to_lines(token.children)
 
-    def render_list_item(self, token: block_token.ListItem) -> str:
-        prefix = "".join((self.indent(), token.leader, " "))
-        prev_indentation = self.indentation
-        self.indentation += " " * (len(token.leader) + 1)
-        self.is_at_beginning_of_line = False
-        content = "".join((prefix, self.render_inner(token), "\n" if not self.is_at_beginning_of_line else ""))
-        self.is_at_beginning_of_line = True
-        self.indentation = prev_indentation
-        return content
+    def render_list_item(self, token: block_token.ListItem) -> Iterable[str]:
+        lines = list(self.block_to_lines(token.children))
+        if len(lines) == 0:
+            lines = [""]
+        return self.prefix_lines(lines, token.leader + " ", " " * (len(token.leader) + 1))
 
     # def render_table(self, token: block_token.Table) -> str:
     #     return self.render_inner(token)
@@ -224,19 +228,17 @@ class MarkdownRenderer(BaseRenderer):
     # def render_table_row(self, token: block_token.TableRow) -> str:
     #     return self.render_inner(token)
 
-    def render_thematic_break(self, token: block_token.ThematicBreak) -> str:
-        content = "".join((self.indent(), token.tag)) # token.tag includes the newline
-        self.is_at_beginning_of_line = True
-        return content
+    def render_thematic_break(self, token: block_token.ThematicBreak) -> Iterable[str]:
+        return [token.tag[:-1]]
 
-    def render_html_block(self, token: block_token.HTMLBlock) -> str:
+    def render_html_block(self, token: block_token.HTMLBlock) -> Iterable[str]:
         lines = token.content[:-1].split("\n")
-        return self.indent_lines(lines)
+        return lines
 
-    def render_link_reference_definition_block(self, token: LinkReferenceDefinitionBlock) -> str:
-        return self.render_inner(token)
+    def render_link_reference_definition_block(self, token: LinkReferenceDefinitionBlock) -> Iterable[str]:
+        return self.block_to_lines(token.children)
 
-    def render_link_reference_definition(self, token: LinkReferenceDefinition) -> str:
+    def render_link_reference_definition(self, token: LinkReferenceDefinition) -> Iterable[str]:
         if token.tag_dest_type == "angle_uri":
             dest_part = "".join(("<", token.dest, ">"))
         else:
@@ -246,13 +248,11 @@ class MarkdownRenderer(BaseRenderer):
             title_part = " {}{}{}".format(token.tag_title, token.title, closer)
         else:
             title_part = ""
-        content = "{}[{}]: {}{}\n".format(self.indent(), token.label, dest_part, title_part)
-        self.is_at_beginning_of_line = True
-        return content
+        content = "[{}]: {}{}".format(token.label, dest_part, title_part)
+        return [content]
 
-    def render_blank_line(self, token: BlankLine) -> str:
-        self.blank_line_emitted = True
-        return "\n" # blank lines should not be indented
+    def render_blank_line(self, token: BlankLine) -> Iterable[str]:
+        return [""]
 
 
 def main():
